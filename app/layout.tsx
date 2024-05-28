3ee850c976932e91dbd4f7cd3518ff32fa003546
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-05-28 23:19:05 +0200

    add shdcnui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import {Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans"
+})
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -18,7 +26,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-sky-100 text-foreground">
+      <body className={cn(
+          "min-h-screen bg-sky-100 font-sans antialiased",
+          fontSans.variable)}>
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
---
+++ app/protected/users/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Page() {
+    return (
+        <div>
+            <h1>Users list</h1>
+            <p>Here is a list of users</p>
+
+        </div>
+    );
+}
+
+export default Page;
